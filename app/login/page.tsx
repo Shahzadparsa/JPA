931ce937cdc4ec0d36d045c99feb59079d41e430
unknown <shahzadpara786@gmail.com>
2023-06-11 12:53:13 +0500

    first commit

---
+++ app/login/page.tsx
@@ -0,0 +1,16 @@
+// import React from 'react'
+
+export const metadata = {
+  title: "Login",
+  description: "Login On JPA",
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  )
+}
+
+export default Login
---
+++ app/signup/page.tsx
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+export const metadata = {
+  title: "Signup",
+  description: "Signup On JPA",
+}
+export default function Signup() {
+  return (
+    <div>
+      <h1>Signup</h1>
+    </div>
+  )
+}
